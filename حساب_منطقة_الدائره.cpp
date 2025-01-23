b59e4a3eeaad6a5e5445d4cc5d6d6b8e405f1005
Soso54 <davedghg193@gmail.com>
2025-01-23 10:11:55 +0200

    Create حساب_منطقة_الدائره.cpp
    برنامج  بسيط يقوم بحساب منطقة الداءره لك بكل سهوله

---
+++ حساب_منطقة_الدائره.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+ double o,k;
+ cout<<"enter~>:";
+ cin>>o;
+ cout<<"\n";
+ cout<<"enter2:~>";
+ cin>>k;
+ cout<<"\n";
+ cout<<"~>"<<"("<<3.14159*k*o<<")";
+ cout<<"\n";
+
+
+
+
+return 0;
+}
